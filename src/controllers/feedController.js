cebc0198b22703992561d516a20bd52edb81dbee
Himanshu Shekhar <shekharhimanshu.8975@gmail.com>
2020-01-16 20:30:28 +0530

    Made changes in file name

--- src/controllers/feedController.js
+++ src/controllers/feedController.js
@@ -21,8 +21,8 @@ class FeedController {
                         "url": config.backendUrl + "?name=" + file,
                         "format": "mp3"
                     };
-                    temp.title = file;
-                    temp.artist = file;
+                    temp.title = file.charAt(0).toUpperCase()+file.slice(1,file.length);
+                    temp.artist = file.charAt(0).toUpperCase()+file.slice(1,file.length);;
 
                     file = file.trim().replace(/ /gi, '-');
                     temp.albumArtUrl = config.backendUrl + "/image/?name=" + file + ".png";
@@ -32,6 +32,7 @@ class FeedController {
             } catch (err) {
                 reject(err);
             }
+            rhymes.sort((a, b) => (a.title >b.title)?1:((b.title>a.title)?-1:0));
             resolve(rhymes)
         })
     }
